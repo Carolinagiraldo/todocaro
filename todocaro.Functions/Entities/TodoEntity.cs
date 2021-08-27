ab4c4dcb2fc55385106093f0165111b2f97722a3
CAROLINA GIRALDO LOPEZ <carolinagiraldo240127@correo.itm.edu.co>
2021-08-27 14:53:54 -0500

    Add entity

---
+++ todocaro.Functions/Entities/TodoEntity.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace todocaro.Functions.Entities
+{
+   public  class TodoEntity : TableEntity
+    {
+        public DateTime createdTime { get; set; }
+
+        public string TaskDescription { get; set; }
+
+        public bool IsCompleted { get; set; }
+    }
+}
---
+++ todocaro.common/Models/todo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace todocaro.common.Models
+{
+    public class todo
+    {
+        public DateTime createdTime { get; set; }
+
+        public  string TaskDescription { get; set; }
+
+        public bool  IsCompleted { get; set; } 
+
+    }
+}
